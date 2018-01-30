58803ba19315f72a9a05099d2c2dbee74e662617
Austin Wright <awguitar86@gmail.com>
2018-01-30 10:34:54 -0700

    made CSS grid practice app

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+
+
+          <header className="header">HEADER</header>
+
+          <div className="hero">HERO</div>
+
+          <div className="body">
+              <div className="item item1">1</div>
+              <div className="item item2">2</div>
+              <div className="item item3">3</div>
+              <div className="item item4">4</div>
+              <div className="item item5">5</div>
+              <div className="item item6">6</div>
+              <div className="item item7">7</div>
+              <div className="item item8">8</div>
+          </div>
+
+          <footer className="footer">FOOTER</footer>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
