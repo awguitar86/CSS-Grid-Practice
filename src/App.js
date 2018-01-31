801244d6612553803bd5b072986caebf045769c6
Austin Wright <awguitar86@gmail.com>
2018-01-31 10:55:09 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Berries from './images/BoxOfChocolateBerries.jpg';
 
 class App extends Component {
   render() {
@@ -12,14 +13,14 @@ class App extends Component {
           <div className="hero"></div>
 
           <div className="body">
-              <img className="item item1"/>
-              <img className="item item2"/>
-              <img className="item item3"/>
-              <img className="item item4"/>
-              <img className="item item5"/>
-              <img className="item item6"/>
-              <img className="item item7"/>
-              <img className="item item8"/>
+              <div className="item item1"></div>
+              <div className="item item2"></div>
+              <div className="item item3"></div>
+              <div className="item item4"></div>
+              <div className="item item5"></div>
+              <div className="item item6"></div>
+              <div className="item item7"></div>
+              <div className="item item8"></div>
           </div>
 
           <footer className="footer">FOOTER</footer>
