3cc7b91aa2f8cdbc250e13d8b7f9dac6707978fa
Austin Wright <awguitar86@gmail.com>
2018-01-31 10:32:49 -0700

    changed div tags to img tags and added a picture

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ class App extends Component {
           <div className="hero"></div>
 
           <div className="body">
-              <div className="item item1">1</div>
-              <div className="item item2">2</div>
-              <div className="item item3">3</div>
-              <div className="item item4">4</div>
-              <div className="item item5">5</div>
-              <div className="item item6">6</div>
-              <div className="item item7">7</div>
-              <div className="item item8">8</div>
+              <img className="item item1"/>
+              <img className="item item2"/>
+              <img className="item item3"/>
+              <img className="item item4"/>
+              <img className="item item5"/>
+              <img className="item item6"/>
+              <img className="item item7"/>
+              <img className="item item8"/>
           </div>
 
           <footer className="footer">FOOTER</footer>
